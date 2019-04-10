d25d6ea0495d50d87eba03bf2d23ad4069f0514e
8bit-number <nastunyamail@gmail.com>
2019-04-10 23:10:05 +0300

    changed code structure

---
+++ file_processor.py
@@ -0,0 +1,10 @@
+# 12,17 ascents - World,Australia,Victoria,North West,Arapiles,Bushranger Bluff,Main Wall ,Horse Drawn
+
+class Ascent:
+    def __init__(self, grade, location, name):
+        self.grade = grade
+        self.location = location
+        self.name = name
+
+    def categorize(self):
+
