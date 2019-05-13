c873a55a163471bee44222f48b5a8fcf071aba85
8bit-number <nastunyamail@gmail.com>
2019-05-13 23:18:00 +0300

    Add python-dotenv

---
+++ application.py
@@ -0,0 +1,56 @@
+import os
+
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+
+from modules.csv_to_db import DataBase
+from modules.countries import locations
+from modules.map_generator import get_mount_coords, get_shop_coords, create_map
+
+app = Flask(__name__, template_folder="templates", static_folder='static')
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+DIFFICULTIES = (
+    "Beginner", "Intermediate", "Experienced", "Expert", "Elite"
+)
+
+db_path = ""
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    countries = locations(db_path)
+
+    if request.method == "GET" and not request.args.get('country'):
+        return render_template(
+            'home.html', locations=countries, difficulties=DIFFICULTIES,
+        )
+
+    country = request.args.get('country')
+    diff = request.args.get("difficulty", None)
+
+    db = DataBase(db_path)
+    if diff:
+        all_data = db.execute_selection_by_difficulty(country, diff)
+    else:
+        all_data = db.execute_selection_by_country(country)
+
+    return render_template("table.html", ascents=all_data)
+
+
+@app.route('/location/<int:route_id>')
+def display_map(route_id):
+    mountain_coordinates = get_mount_coords(route_id)
+    shop_coordinates = get_shop_coords(mountain_coordinates)
+    create_map(mountain_coordinates, shop_coordinates)
+    return render_template("map.html")
+
+
+if __name__ == '__main__':
+    APP_ROOT = os.path.dirname(__file__)
+
+    dotenv_path = os.path.join(APP_ROOT, '.env')
+    load_dotenv(dotenv_path)
+
+    db_path = os.getenv("DB_PATH")
+    app.run(debug=True)
--- modules/countries_db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-with sqlite3.connect(
-        "/home/nastya/PycharmProjects/course_work/data/locations.db") as conn:
-    cursor = conn.cursor()
-    cursor.execute("SELECT DISTINCT location FROM locations")
-    countries = [i[0] for i in cursor.fetchall()]
